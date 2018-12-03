d6d6571484f8f96f80ee0226893429029b026a89
Unknown <todaudrhdgkr@gmail.com>
2018-12-03 20:44:06 +0900

    타이틀화면 수정

---
+++ rotten_tomatos/Assets/Scripts/titlemanager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class titlemanager : MonoBehaviour {
+    public GameObject R;
+    public GameObject o;
+    public GameObject t1;
+    public GameObject t2;
+    public GameObject e;
+    public GameObject n;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
